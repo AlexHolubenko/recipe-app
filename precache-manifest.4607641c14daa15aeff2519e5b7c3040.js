f2f0702a86d710f98678405afb7942452e1da42c
Oleksandr Holubenko <naturlichgood@gmail.com>
2020-03-21 23:56:23 +0200

    Updates

---
+++ precache-manifest.4607641c14daa15aeff2519e5b7c3040.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629bb1c3acd5a847e7364a813947a907",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "222d34dc99c9b34b6e67",
+    "url": "/recipe-app/static/css/main.90ffba0a.chunk.css"
+  },
+  {
+    "revision": "4cadb7b67587f21d2dd0",
+    "url": "/recipe-app/static/js/2.e1e678d0.chunk.js"
+  },
+  {
+    "revision": "222d34dc99c9b34b6e67",
+    "url": "/recipe-app/static/js/main.8b00b6fb.chunk.js"
+  },
+  {
+    "revision": "6aa3a604642fe05590e2",
+    "url": "/recipe-app/static/js/runtime-main.2fd4c751.js"
+  }
+]);
